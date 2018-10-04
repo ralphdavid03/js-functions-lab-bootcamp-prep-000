eea5e1d6570f7bfc445ccbc6af68f87a537fabcb
Ralph David Javier <ralphdavid_08_03@yahoo.com>
2018-10-04 05:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+var holiday;
+var days;
+var dog;
+
+function happyHolidays(){
+  return `Happy Holidays`;
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}`;
+}
+
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}`;
+}
+
+function holidayCountdown(days,holiday){
+  return `It's ${days} days until ${holiday}!`;
+}
